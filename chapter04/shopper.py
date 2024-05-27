a9259f9f99d535180022786289f384a001c8a5ef
blossun <blossunn@gmail.com>
2024-05-28 01:38:31 +0900

    추적 데이터 생성 - 두 번쨰 스팬 생성

--- chapter04/shopper.py
+++ chapter04/shopper.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from opentelemetry import trace
+from opentelemetry import context, trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
@@ -17,5 +17,10 @@ def configure_tracer():
 if __name__ == "__main__":
     tracer = configure_tracer()
     span = tracer.start_span("visit store") # 스팬 생성
+    ctx = trace.set_span_in_context(span) # 스팬을 컨텍스트에 할당하여 스팬을 활성화 - 두 번째 스팬 시작 전 컨텍스트 지정을 위해 사용할 컨텍스트 객체를 return해준다.
+    token = context.attach(ctx) # 전달된 컨텍스트 인자로 현재의 컨텍스트를 지정. 응답 값은 고유의 토큰
+    span2 = tracer.start_span("browse")
     browse()
+    span2.end()
+    context.detach(token) # 컨텍스트를 이전 상태로 되돌리기
     span.end() # 작업 완료 후 end 호출
