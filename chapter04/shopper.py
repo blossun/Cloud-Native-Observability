893929902210a1da861cac0940f3c3c027d88800
blossun <blossunn@gmail.com>
2024-05-28 17:56:29 +0900

    스팬 유형 지정 및 컨텍스트 전파

--- chapter04/shopper.py
+++ chapter04/shopper.py
@@ -1,29 +1,10 @@
 #!/usr/bin/env python3
-from opentelemetry import context, trace
-from opentelemetry.sdk.resources import Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter, SimpleSpanProcessor
-from opentelemetry.semconv.trace import HttpFlavorValues, SpanAttributes
-from local_machine_resource_detector import LocalMachineResourceDetector
+import requests
+from opentelemetry import trace
+from opentelemetry.semconv.trace import SpanAttributes
+from opentelemetry.propagate import inject
 
-
-def configure_tracer(name, version):
-    exporter = ConsoleSpanExporter()
-    span_processor = SimpleSpanProcessor(exporter)  # 스팬처리기 변경
-    local_resource = LocalMachineResourceDetector().detect()
-    resource = local_resource.merge(
-        Resource.create(
-            {
-                "service_name": name,
-                "service_version": version,
-            }
-        )
-    )
-    provider = TracerProvider(resource=resource)
-    provider.add_span_processor(span_processor)
-    trace.set_tracer_provider(provider)
-    return trace.get_tracer(name, version)  # 추적기 획득 (계측모듈 이름, 버전)
-
+from common import configure_tracer
 
 tracer = configure_tracer("shopper", "0.1.2")  # 추적기 인스턴스를 전역으로 설정
 
@@ -41,13 +22,20 @@ def add_item_to_cart(item, quantity=1):
 @tracer.start_as_current_span("browse")
 def browse():
     print("visiting the grocery store")
-    span = trace.get_current_span()  # 현재 스팬을 얻어옴
-    span.set_attributes({
-        SpanAttributes.HTTP_METHOD: "GET",
-        SpanAttributes.HTTP_FLAVOR: "1.1",
-        SpanAttributes.HTTP_URL: "http://localhost:5000",
-        SpanAttributes.NET_PEER_IP: "127.0.0.1",
-    })
+    with tracer.start_as_current_span(  # 컨텍스트 매니저는 web request라는 이름의 새로운 스팬을 CLIENT로 지정해서 시작
+            "web request", kind=trace.SpanKind.CLIENT
+    ) as span:
+        url = "http://localhost:5000"  # grocery-store
+        span.set_attributes({
+            SpanAttributes.HTTP_METHOD: "GET",
+            SpanAttributes.HTTP_FLAVOR: "1.1",
+            SpanAttributes.HTTP_URL: url,
+            SpanAttributes.NET_PEER_IP: "127.0.0.1",
+        })
+    headers = {}
+    inject(headers)  # HTTP 요청의 헤더로 전달될 딕셔너리 객체를 span_context에 설정
+    resp = requests.get(url, headers=headers)
+    span.set_attribute(SpanAttributes.HTTP_STATUS_CODE, resp.status_code)  # 응답 정보도 스팬 속성에 넣어주자
     add_item_to_cart("orange", 5)
 
 
