38080137e6ea8e26de07fda9c880a495dba3a7c7
blossun <blossunn@gmail.com>
2024-05-29 14:42:35 +0900

    익스포터를 설정
    - 애플리케이션이 생성된 메트릭에 대해 어떤 작업을 수행할지 알려주기 위해 익스포터를 설정
    - 주기적으로 메트릭을 추출하기 위해 PeriodicExportingetricReader를 설정
    - MetricProvider에 MetricReader를 추가

--- chapter05/metrics.py
+++ chapter05/metrics.py
@@ -1,10 +1,17 @@
 from opentelemetry.metrics import set_meter_provider
 from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import (
+    ConsoleMetricExporter,
+    PeriodicExportingMetricReader,
+)
 from opentelemetry.sdk.resources import Resource
 
 
 def configure_meter_provider():
-    provider = MeterProvider(resource=Resource.create())
+    exporter = ConsoleMetricExporter()  # 메트릭을 콘솔로 출력해주는 익스포터 설정
+
+    reader = PeriodicExportingMetricReader(exporter=exporter, export_interval_millis=5000)  # 5초 주기로 메트릭을 추출
+    provider = MeterProvider(metric_readers=[reader], resource=Resource.create())  # MetricProvider에 MetricReader를 추가
     set_meter_provider(provider)
 
 
