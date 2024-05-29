4e7a1e275e9707f5e4214eced78faef12c3572ea
blossun <blossunn@gmail.com>
2024-05-29 21:23:04 +0900

    메트릭 계측기 1 - 카운터

--- chapter05/metrics.py
+++ chapter05/metrics.py
@@ -1,4 +1,3 @@
-from opentelemetry.exporter.prometheus import PrometheusMetricReader
 from opentelemetry.metrics import set_meter_provider, get_meter_provider
 from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.metrics.export import (
@@ -6,12 +5,10 @@ from opentelemetry.sdk.metrics.export import (
     PeriodicExportingMetricReader,
 )
 from opentelemetry.sdk.resources import Resource
-from prometheus_client import start_http_server
 
 
 # 전역 MeterProvider 설정
 def configure_meter_provider():
-    start_http_server(port=8000, addr="localhost")  # 8000 포트로 프로메테우스 엔드포인트를 노출 (프로메테우스 클라이언트 라이브러리가 제공)
     exporter = ConsoleMetricExporter()  # 메트릭을 콘솔로 출력해주는 익스포터 설정
     reader = PeriodicExportingMetricReader(exporter=exporter, export_interval_millis=5000)  # 5초 주기로 메트릭을 추출
     provider = MeterProvider(metric_readers=[reader], resource=Resource.create())  # MetricProvider에 MetricReader를 추가
@@ -27,4 +24,11 @@ if __name__ == "__main__":
         version="0.1.2",
         schema_url="https://opentelemetry.io/schemas/1.9.0"
     )
+    counter = meter.create_counter(
+        "items_sold",
+        unit="items",
+        description="Total items sold"
+    )
+    counter.add(6, {"locale": "fr-FR", "country": "CA"})
+    counter.add(1, {"locale": "es-ES"})  # counter는 음수를 넣을 수 없음
     input("Press any key to exit...")
