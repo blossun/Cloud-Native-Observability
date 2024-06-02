0492b53d5f10b40f8da85125fb1302d7540838e9
blossun <blossunn@gmail.com>
2024-06-02 15:53:24 +0900

    OpenTelemetry API로 로거를 생성

--- chapter06/logs.py
+++ chapter06/logs.py
@@ -1,4 +1,4 @@
-from opentelemetry._logs import set_logger_provider
+from opentelemetry._logs import set_logger_provider, get_logger_provider
 from opentelemetry.sdk._logs import LoggerProvider
 from opentelemetry.sdk._logs._internal.export import ConsoleLogExporter, BatchLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
@@ -9,3 +9,11 @@ def configure_logger_provider():
     set_logger_provider(provider)  # 전역 Logger로 설정
     exporter = ConsoleLogExporter()
     provider.add_log_record_processor(BatchLogRecordProcessor(exporter))
+
+
+if __name__ == "__main__":
+    configure_logger_provider()
+    logger = get_logger_provider().get_logger(  # OpenTelemetry API로 로거를 생성
+        "shopper",
+        instrumenting_module_verison="0.1.2",
+    )
